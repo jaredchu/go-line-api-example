c0932f8f755292eb74cbf131ae48080f4fdec466
Jared Chu <me@jaredchu.com>
2018-05-10 10:40:23 +0700

    add check last request

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/memcache"
+	"fmt"
+	"io"
+	"encoding/json"
 )
+
+var memCacheKey = "last_request"
 
 func init() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/webhook", webhook)
+	http.HandleFunc("/webhook/list", webhookList)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +24,36 @@ func home(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func login(w http.ResponseWriter, r *http.Request)  {
+func login(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/login.html"))
 	tmpl.Execute(w, nil)
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	data, err := json.Marshal(r.Form)
+	if (err != nil) {
+		io.WriteString(w, "Error")
+	}
+
+	ctx := appengine.NewContext(r)
+	item := &memcache.Item{
+		Key:   memCacheKey,
+		Value: data,
+	}
+	if err := memcache.Set(ctx, item); err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+
+	io.WriteString(w, string(data))
+}
+
+func webhookList(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	if item, err := memcache.Get(ctx, memCacheKey); err == memcache.ErrCacheMiss {
+		io.WriteString(w, "No item")
+	} else {
+		io.WriteString(w, string(item.Value[:]))
+	}
 }
